941dc93578f522aee513520ef012c1f264195238
Sagar Thapa <s_thapa119687@fanshaweonline.ca>
2022-10-21 21:43:23 -0400

    Home and register activity added

--- app/src/main/java/com/example/medreminder/MainActivity.java
+++ app/src/main/java/com/example/medreminder/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.medreminder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +14,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void Register(View view)
+    {
+        //Open the Register activity
+
+        //passes the value to the second activity through the intent
+        Intent intent = new Intent(this,RegisterActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void Login(View view)
+    {
+        //Open the Register activity
+
+        //passes the value to the second activity through the intent
+        Intent intent = new Intent(this,HomeActivity.class);
+        startActivity(intent);
+
+    }
 }
