99b717aa71be7ef3f843a2ffc486c8991e57c43f
Sagar Thapa <s_thapa119687@fanshaweonline.ca>
2022-10-24 16:50:59 -0400

    Added a Dialog class and layout to add Reminder

--- app/src/main/java/com/example/medreminder/HomeActivity.java
+++ app/src/main/java/com/example/medreminder/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.medreminder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,4 +13,27 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
+    //open a new activity when menu is clicked.
+    public void onMenu(View view)
+    {
+        //if valid goes to the home activity.
+        Intent intent = new Intent(this,MenuActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_in_left);
+
+    }
+
+    //TODO
+    public void onReminder(View view)
+    {
+
+    }
+
+    //TO:DO
+    public void openMedications(View view)
+    {
+
+    }
+
 }
---
+++ app/src/main/java/com/example/medreminder/ReminderDialog.java
@@ -0,0 +1,8 @@
+package com.example.medreminder;
+
+import androidx.core.app.DialogCompat;
+
+public class ReminderDialog{
+
+
+}
